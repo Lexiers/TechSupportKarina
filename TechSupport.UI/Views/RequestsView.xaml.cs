f31a5390430f29c097f72e3d240a36509a45ac30
Lexiers <96147748+Lexiers@users.noreply.github.com>
2024-04-25 23:58:41 +0500

    Добавьте файлы проекта.

---
+++ TechSupport.UI/Views/RequestsView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using HandyControl.Controls;
+using TechSupport.UI.ViewModels;
+
+namespace TechSupport.UI.Views;
+
+public partial class RequestsView : Window
+{
+    public RequestsView(RequestsViewModel context)
+    {
+        InitializeComponent();
+        DataContext = context;
+    }
+}
